223ed2bb18138f55734252688e09f71dd6fd857a
Gabriel Tucker <gabeqtucker@gmail.com>
2025-11-28 16:48:03 -0500

    visibility correction

--- game.bat
+++ game.bat
@@ -1,2 +1,7 @@
 @echo off
-dotnet run
+setlocal
+cd /d "%~dp0"
+powershell -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -Command ^
+  "Start-Process -WindowStyle Hidden -FilePath 'dotnet' -ArgumentList 'run' -WorkingDirectory '%cd%'"
+endlocal
+exit /b
